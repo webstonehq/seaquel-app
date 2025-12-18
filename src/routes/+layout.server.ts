2e4f0e397567cd0659fd62640715aaa58f387b1d
Mike Nikles <788827+mikenikles@users.noreply.github.com>
2025-12-18 08:47:58 -0800

    Add Cache-Control HTTP headers

---
+++ src/routes/+layout.server.ts
@@ -0,0 +1,17 @@
+import { dev } from "$app/environment";
+import type { LayoutServerLoad } from "./$types";
+
+const ONE_MINUTE = 60;
+const FIFTEEN_MINUTES = ONE_MINUTE * 15;
+const ONE_HOUR = ONE_MINUTE * 60;
+const ONE_DAY = ONE_HOUR * 24;
+
+export const load: LayoutServerLoad = ({ setHeaders }) => {
+  if (!dev) {
+    setHeaders({
+      "Cache-Control": `public, max-age=${FIFTEEN_MINUTES}, s-maxage=${ONE_DAY}`,
+      Vary: "Accept-Encoding",
+    });
+  }
+  return {};
+};
