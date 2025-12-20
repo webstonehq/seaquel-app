65c6df6ef52595e208d6e84b4e2b17c425e731fb
Mike Nikles <788827+mikenikles@users.noreply.github.com>
2025-12-20 09:39:58 -0800

    Find the latest GitHub release and return its latest.json for the app updater to use

--- src/routes/updates/check/[target]/[arch]/[current_version]/+server.ts
+++ src/routes/updates/check/[target]/[arch]/[current_version]/+server.ts
@@ -1,17 +1,56 @@
 import { json } from "@sveltejs/kit";
 
-export const GET = ({ params }) => {
-  console.log("Checking for available updates.", {params});
-  const isUpdateAvailable = Math.random() < 0.5;
-  if (isUpdateAvailable) {
-    json({
-      version: "",
-      pub_date: "",
-      url: "",
-      signature: "",
-      notes: "",
-    });
-  } else {
+export const GET = async ({ params }) => {
+  console.log("Checking for available updates.", { params });
+
+  try {
+    const releaseResponse = await fetch(
+      "https://api.github.com/repos/webstonehq/seaquel/releases", {
+        headers: {
+          "User-Agent": "seaquel-app-updates-checker"
+        }
+      },
+    );
+
+    if (!releaseResponse.ok) {
+      console.error("Failed to fetch releases: ", await releaseResponse.text());
+      return new Response(null, { status: 204 });
+    }
+
+    const releases = await releaseResponse.json();
+    console.log(`Found ${releases.length} releases`);
+
+    // Find the latest release that is not a draft and not a prerelease
+    const validRelease = releases.find(
+      (release: { draft: boolean; prerelease: boolean }) =>
+        !release.draft && !release.prerelease,
+    );
+    console.log(`Found latest release: `, validRelease);
+
+    if (!validRelease) {
+      return new Response(null, { status: 204 });
+    }
+
+    const latestJsonAsset = validRelease.assets?.find(
+      (asset: { name: string }) => asset.name === "latest.json",
+    );
+
+    if (!latestJsonAsset) {
+      console.error("Failed to find latest.json asset in release");
+      return new Response(null, { status: 204 });
+    }
+
+    const assetResponse = await fetch(latestJsonAsset.browser_download_url);
+
+    if (!assetResponse.ok) {
+      console.error("Failed to fetch latest.json content: ", await assetResponse.text());
+      return new Response(null, { status: 204 });
+    }
+
+    const latestJson = await assetResponse.json();
+    return json(latestJson);
+  } catch (error) {
+    console.error(error);
     return new Response(null, { status: 204 });
   }
 };
