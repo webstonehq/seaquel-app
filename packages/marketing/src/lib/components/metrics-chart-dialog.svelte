35aa39fdffdea55b17a790b8c1ff3d1273850302
Mike Nikles <788827+mikenikles@users.noreply.github.com>
2026-02-21 17:05:33 +0900

    Improve metrics charts on mobile

--- packages/marketing/src/lib/components/metrics-chart-dialog.svelte
+++ packages/marketing/src/lib/components/metrics-chart-dialog.svelte
@@ -24,28 +24,30 @@
 </script>
 
 <Dialog.Root bind:open>
-	<Dialog.Content class="sm:max-w-2xl">
+	<Dialog.Content class="max-w-[calc(100%-1rem)] sm:max-w-2xl max-h-[90dvh] overflow-x-hidden overflow-y-auto p-4 sm:p-6">
 		<Dialog.Header>
 			<Dialog.Title>{title}</Dialog.Title>
 			<Dialog.Description>Downloads broken down by release</Dialog.Description>
 		</Dialog.Header>
-		{#if data.length > 0}
-			<ChartContainer config={chartConfig} class="h-[350px] w-full">
-				<BarChart
-					data={data}
-					x="label"
-					y="value"
-					padding={{ top: 20, right: 20, bottom: 80, left: 50 }}
-					props={{
-						bars: { fill: barColor, radius: 4 },
-						xAxis: { tickLabelProps: { rotate: -45, textAnchor: "end" } },
-					}}
-				/>
-			</ChartContainer>
-		{:else}
-			<div class="flex h-[350px] items-center justify-center text-muted-foreground">
-				No download data available
-			</div>
-		{/if}
+		<div class="min-w-0 overflow-hidden">
+			{#if data.length > 0}
+				<ChartContainer config={chartConfig} class="h-[250px] sm:h-[350px] w-full">
+					<BarChart
+						data={data}
+						x="label"
+						y="value"
+						padding={{ top: 20, right: 10, bottom: 60, left: 40 }}
+						props={{
+							bars: { fill: barColor, radius: 4 },
+							xAxis: { tickLabelProps: { rotate: -45, textAnchor: "end" } },
+						}}
+					/>
+				</ChartContainer>
+			{:else}
+				<div class="flex h-[250px] sm:h-[350px] items-center justify-center text-muted-foreground">
+					No download data available
+				</div>
+			{/if}
+		</div>
 	</Dialog.Content>
 </Dialog.Root>
