d3efd4b39b5cb986289a430dd229627e6d22903d
Mike Nikles <788827+mikenikles@users.noreply.github.com>
2026-04-10 16:31:21 +0900

    Make the /discord vanity URL a permanent redirect

--- packages/marketing/src/routes/discord/+server.ts
+++ packages/marketing/src/routes/discord/+server.ts
@@ -2,5 +2,5 @@ import { redirect } from "@sveltejs/kit";
 import type { RequestHandler } from "./$types";
 
 export const GET: RequestHandler = () => {
-	redirect(302, "https://discord.gg/QuzUESE8x4");
+	redirect(301, "https://discord.gg/QuzUESE8x4");
 };
